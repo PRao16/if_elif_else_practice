b4bacb2f03270f783561292860d0f20299079612
PRao16 <48734576+PRao16@users.noreply.github.com>
2023-06-02 06:28:54 -0500

    Add files via upload

---
+++ If_elif_else.py
@@ -0,0 +1,16 @@
+a = 25
+b = 75
+
+total = a + b
+
+if total >= 125:
+    print("a plus b is greater than 125")
+
+elif total >= 150:
+    print("a plus be is greater than 150")
+
+elif total >= 180:
+    print("a plus b is greater than 180")
+
+else:
+    print("a plus b is not greater than 125")
